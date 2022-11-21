77966f6611351fc6ff341bb4befd5793dad77f73
Aurelio Gisler <aurelio.gisler@xippo.ch>
2022-11-21 10:06:15 +0100

    bug(vertragspartner): anteil wurde nicht korrekt gerechnet

--- Vertragspartner.sql
+++ Vertragspartner.sql
@@ -6,7 +6,7 @@ CASE WHEN utab1.partner_id != 1 THEN utab1.partner_id + 100 ELSE TO_NUMBER(utab1
 utab1.type,
 'false' AS ForCooperative,
 1 AS MandatoryPortionType,
-1 / (NVL(utab2.rowCounter, 0) + 1) AS MandatoryPortionAmount
+ROUND(1 / (NVL(utab2.rowCounter, 0) + 1), 2) AS MandatoryPortionAmount
 
 FROM
 TEMP10
@@ -31,10 +31,14 @@ FROM
 TEMP10 
 INNER JOIN solidarmieter ON solidarmieter.partner_id = TEMP10.partner_id AND TEMP10.mv = solidarmieter.mv 
 INNER JOIN partner ON partner.partner_id = solidarmieter.i_partner_id 
+INNER JOIN mv ON mv.partner_id = solidarmieter.partner_id AND mv.mv = solidarmieter.mv 
+
+WHERE NVL(mv.endedat, '31.12.9999') > SYSDATE
 ) 
 
 utab1 ON utab1.main_id = TEMP10.ID 
-LEFT OUTER JOIN (SELECT COUNT(*) AS rowCounter, solidarmieter.partner_id FROM solidarmieter INNER JOIN mv ON mv.partner_id = solidarmieter.partner_id AND mv.mv = solidarmieter.mv WHERE NVL(mv.endedat, '31.12.9999') > SYSDATE GROUP BY solidarmieter.partner_id) utab2 ON utab2.Partner_id = TEMP10.partner_id
+
+LEFT OUTER JOIN (SELECT COUNT(*) AS rowCounter, solidarmieter.partner_id, solidarmieter.mv FROM solidarmieter INNER JOIN mv ON mv.partner_id = solidarmieter.partner_id AND mv.mv = solidarmieter.mv WHERE NVL(mv.endedat, '31.12.9999') > SYSDATE GROUP BY solidarmieter.partner_id, solidarmieter.mv) utab2 ON utab2.Partner_id = TEMP10.partner_id AND utab2.mv = TEMP10.mv
 
 ORDER BY 
 Temp10.id,
